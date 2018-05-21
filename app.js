9e560dece8c34df9d2e5a6d1a8a3e1ad3b4af88f
Jatin Patwa <jatinpatwa401@gmail.com>
2018-05-22 00:36:49 +0530

    Initial. File upload and custom directories working

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+   res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+
+
+app.post('/upload:userName', function(req, res){
+
+
+  var username = req.params.userName
+  mkdirp(path.join(__dirname, '/uploads/'+username), function (err) {
+    if (err) console.error(err)
+    else console.log(username)
+  });
+  // console.log(username)
+
+
+
+  var form = new formidable.IncomingForm();
+
+
+  form.multiples = true;
+
+  form.uploadDir = path.join(__dirname, '/uploads/'+username);
+
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+ form.on('field', function(field, userName) {
+ console.log(userName);
+});
+
+
+
+
+
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+
+  form.on('end', function() {
+
+    res.end('success');
+  });
+
+  form.parse(req);
+
+});
+
+ var server = app.listen(3000, function(){
+ console.log('Server listening on port 3000');
+});
