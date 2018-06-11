abe8f9ad2e8cde9dd2b72be6143c17d52f32e195
Jatin Patwa <jatinpatwa401@gmail.com>
2018-06-11 23:28:25 +0530

    new release

--- app.js
+++ app.js
@@ -1,124 +1,186 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-// var XML = require('pixl-xml');
-var xml2js = require('xml2js');
-var nodemailer = require('nodemailer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const nodemailer = require('nodemailer');
 
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-   res.sendFile(path.join(__dirname, 'views/index.html'));
+// Mongo URI
+const mongoURI = 'mongodb://jatin:fileupload0@ds119772.mlab.com:19772/landysgyruploads';
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
 });
 
-
-// var doc = null;
-// try {
-// 	doc = XML.parse(__dirname, '/uploads/test.xml', {preserveAttributes: true});
-// }
-// catch (err) {
-// 	console.log("XML Parser Error: " + err);
-// }
-// console.log(doc);
-
-
-
-
-
-
-app.post('/upload:userName', function(req, res){
-
-  var parser = new xml2js.Parser();
-  fs.readFile(__dirname + '/uploads/test.xml', function(err, data) {
-    var parsed = parser.parseString(data, function (err, result) {
-        console.dir(result);
-        console.log('Done');
-        var transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true,
-          service: 'gmail',
-          auth: {
-            user: 'coffinskull137@gmail.com',
-            pass: 'password'
-          }
-        });
-
-        var mailOptions = {
-          from: 'coffinskull137@gmail.com',
-          to: 'jatinpatwa401@gmail.com',
-          subject: 'Sending Email using Node.js',
-          // alternatives: result
-          text: 'Sent from NodeMailer'
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+        const ip = req.connection.remoteAddress
+        const filename = 'file_' + ip + '_' + file.originalname;
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
         };
-
-        transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-
-    });
-  });
-
-  var ip = req.connection.remoteAddress
-  var username = req.params.userName
-  console.log(ip)
-
-  if (!fs.existsSync(path.join(__dirname, '/uploads/'+username))){
-    mkdirp(path.join(__dirname, '/uploads/'+username), function (err) {
-      if (err) console.error(err)
-      else console.log(username)
+        resolve(fileInfo);
     });
   }
-  else console.log('Folder already exists, upload successful');
-  // console.log(username)
+});
+const upload = multer({ storage });
 
-
-
-  var form = new formidable.IncomingForm();
-
-
-  form.multiples = true;
-
-  form.uploadDir = path.join(__dirname, '/uploads/'+username);
-
-  form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, ip + '_' + file.name));
+// @route GET /
+// @desc Loads form
+app.get('/', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render('index', { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === 'image/jpeg' ||
+          file.contentType === 'image/png'
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.render('index', { files: files });
+    }
   });
-
- form.on('field', function(field, userName) {
- console.log(userName);
 });
 
+// @route POST /upload
+// @desc  Uploads file to DB
+app.post('/upload', upload.single('file'), (req, res) => {
+  //console.log(res.json({ file: req.file }));
+  res.redirect('/');
+});
 
+// @route GET /files
+// @desc  Display all files in JSON
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      });
+    }
 
+    // Files exist
+    return res.json(files);
+  });
+});
 
+// @route GET /files/:filename
+// @desc  Display single file object
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+    // File exists
+    return res.json(file);
+  });
+});
 
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
+// // @route GET /image/:filename
+// // @desc Display Image
+// app.get('/image/:filename', (req, res) => {
+//   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+//     // Check if file
+//     if (!file || file.length === 0) {
+//       return res.status(404).json({
+//         err: 'No file exists'
+//       });
+//     }
+//
+//     // Check if image
+//     if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+//       // Read output to browser
+//       const readstream = gfs.createReadStream(file.filename);
+//       readstream.pipe(res);
+//     } else {
+//       res.status(404).json({
+//         err: 'Not an image'
+//       });
+//     }
+//   });
+// });
+
+// @route DELETE /files/:id
+// @desc  Delete file
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect('/');
+  });
+});
+
+// @route PARSE /parse/:filename
+// @desc Parses and emails
+
+app.get('/parse/:filename', (req, res) => {
+
+  
+
+  var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        service: 'gmail',
+        auth: {
+          user: 'coffinskull137@gmail.com',
+          pass: 'dwijatin'
+        }
+      });
+      var mailOptions = {
+        from: 'coffinskull137@gmail.com',
+        to: 'jatinpatwa401@gmail.com',
+        subject: 'Sending Email using Node.js',
+        // alternatives: result
+        text: 'Sent from NodeMailer'
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    res.redirect('/');
   });
 
 
-  form.on('end', function() {
 
-    res.end('success');
-  });
+const port = 5000;
 
-  form.parse(req);
-
-});
-
-
-
-
-
- var server = app.listen(3000, function(){
- console.log('Server listening on port 3000');
-});
+app.listen(port, () => console.log(`Server started on port ${port}`));
